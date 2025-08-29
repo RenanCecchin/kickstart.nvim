069a4b2ba35e1a5360db4bfff492feb57772c61e
RenanCecchin <renan.cecchin@hotmail.com>
2025-08-29 09:20:20 -0300

    config tweaks

--- init.lua
+++ init.lua
@@ -779,7 +779,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff_format' },
+        python = { 'ruff_format', 'isort' },
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
