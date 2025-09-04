55d94239226a357f5f90f9bac3768b3b71476f42
RenanCecchin <renan.cecchin@hotmail.com>
2025-09-04 10:42:10 -0300

    added catppuccin theme and new way to configure theme in init.lua

--- init.lua
+++ init.lua
@@ -100,6 +100,7 @@ require 'keymaps'
 require 'lazy-bootstrap'
 
 require 'lazy-plugins'
+vim.cmd.colorscheme 'catppuccin'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,3 @@
+return {
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, opts = { dim_inactive = { enabled = true, percentage = 0.07 }, auto_integrations = true } },
+}
