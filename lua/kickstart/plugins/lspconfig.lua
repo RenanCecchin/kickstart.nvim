c65f94d7222a7beb6b23de1df014c562b8d1ef47
RenanCecchin <renan.cecchin@hotmail.com>
2025-09-04 15:07:29 -0300

    added comment for section splitting

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -207,9 +207,9 @@ return { -- LSP Plugins
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- C
         clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        -- Python
         ruff = {},
         ty = {
           inlayHints = {
@@ -219,17 +219,9 @@ return { -- LSP Plugins
             rename = true,
           },
         },
+        -- Zig
         zls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        -- Lua
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
